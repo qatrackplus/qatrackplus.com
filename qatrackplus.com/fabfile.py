f8a0547b6b1259536f936814f0a39ac630a052d7
Randle Taylor <randle.taylor@gmail.com>
2014-05-11 20:04:27 -0400

    add sitemap to fabfile

---
+++ qatrackplus.com/fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import run, env, cd, local
+
+
+env.hosts = ['randlet.com']
+
+def deploy():
+    local("git push origin master")
+    with cd("~/projects/qatrackplus.com"):
+        run("git pull origin master")
+        run("rm -rf deploy && mkdir deploy")
+        run("cp -r img css fonts js index.html sitemap.xml deploy")
+
+
+
+
