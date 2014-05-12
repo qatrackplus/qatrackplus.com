670cf39782ebeae23b05370914086eb932ebd93f
Randle Taylor <randle.taylor@gmail.com>
2014-05-11 20:09:59 -0400

    fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ def deploy():
     with cd("~/projects/qatrackplus.com"):
         run("git pull origin master")
         run("rm -rf deploy && mkdir deploy")
-        run("cp -r qatrackplus.com/{img,css,fonts,js,index.html} deploy")
+        run("cp -r qatrackplus.com/{img,css,fonts,js,index.html,sitemap.xml} deploy")
 
 
 
