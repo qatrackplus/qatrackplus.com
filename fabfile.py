271ffebaa4ee86cc48c2b088485926aa9831cd34
randlet <randle.taylor@gmail.com>
2019-10-25 10:33:07 -0400

    add publications to fab file

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,4 @@
-from fabric.api import run, env, cd, local
-
+from fabric.api import cd, env, local, run
 
 env.hosts = ['randlet.com']
 
@@ -8,8 +7,5 @@ def deploy():
     with cd("~/projects/qatrackplus.com"):
         run("git pull origin master")
         run("rm -rf deploy && mkdir deploy")
-        run("cp -r qatrackplus.com/{img,css,fonts,js,index.html,news.html,sitemap.xml} deploy")
-
-
-
+        run("cp -r qatrackplus.com/{img,css,fonts,js,index.html,news.html,publications.html,sitemap.xml} deploy")
 
